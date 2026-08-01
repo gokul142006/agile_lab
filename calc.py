2d6e35f293c7cb52b26e2dba3293499568eee49b
gokul142006 <gokulsivasubramani@Gokuls-MacBook-Air.local>
2026-08-01 21:55:33 +0530

    sub fuction added

--- calc.py
+++ calc.py
@@ -4,7 +4,11 @@ def add(a,b) :
 def sub(a,b) :
     return (a-b)
 
+def mul(a,b) :
+    return (a*b)
+
 x = 10
 y = 10
 print("the sum of two numbers " , add(x,y))
 print("the diff of two numbers " , sub(x,y))
+print("the mul of two numbers " , mul(x,y))
